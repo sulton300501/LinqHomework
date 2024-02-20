46b578d0cfec52605edf32911a2c56616f105479
sulton <sultonmalik326@gmail.com>
2024-02-21 01:55:02 +0500

    Add project files.

---
+++ LinqHomework/Model/Buxgalters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqHomework.Model
+{
+    public class Buxgalters
+    {
+        public int Id { get; set; }
+        public string BName { get; set; }
+       
+    }
+}
---
+++ LinqHomework/Program.cs
@@ -0,0 +1,45 @@
+﻿using LinqHomework.Model;
+
+namespace LinqHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var progLang = new List<ProgrammingLanguage>()
+            { 
+              new ProgrammingLanguage {Id=1, Name="C++",BuxgaltersId=1},
+              new ProgrammingLanguage {Id=2,Name="Javascript",BuxgaltersId=2},
+              new ProgrammingLanguage {Id=3,Name="Python", BuxgaltersId = 1}
+            };
+
+
+
+            var buxgalter = new List<Buxgalters>()
+            {
+              new Buxgalters {Id=1, BName="Sulton"},
+              new Buxgalters {Id=2,BName="Adham"},
+              new Buxgalters {Id=3,BName="Jamshid"}
+            };
+
+
+
+            var countLang = progLang.Join(buxgalter, prg => prg.BuxgaltersId,
+             buxg => buxg.Id, (prg, buxg) => new { prg.Name, buxg.BName });
+
+            Console.WriteLine("Count Lang: ");
+
+            foreach (var item in countLang)
+            {
+                Console.WriteLine(item.Name + " " + item.BName);
+            }
+
+
+
+
+
+
+        }
+    }
+}
